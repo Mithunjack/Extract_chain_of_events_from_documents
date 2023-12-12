18838799893575fadb32a98f731917d75d2f0899
aaa006bd <aaa006bd@gmail.com>
2023-12-12 16:40:15 +0100

    chore: updated prompt

--- app/backend/prompts.py
+++ app/backend/prompts.py
@@ -2,7 +2,7 @@ basic_event_prompt_template = 'Format the response in json format with key calle
 
 
 event_instruction_template = '''
-If your answer contains  a series of events format it as a JSON list where each entry is a json object with the following keys: time(time of event), title and body
+If your answer contains  a series of events format it as a JSON list where each entry is a json object with the following keys: time(when the event occurred), title(title of the event or incident) and body
 ).
 If the answer for the user doesn't contain any chain of events then just answer them.
 '''
